Classifier trained. Precision, Recall, and F1 are always 1.00. Something is terribly, terribly wrong.

model.py
@@ -1,14 +1,41 @@
-import sys
-import os
 import _pickle
+import json
+import numpy as np
+import os
+import sklearn
+import sys
+
+from scipy.sparse import csr_matrix
 
 from scripts.convert import *
+from scripts.train import *
 from features.features import *
 from corpora import *
 
+MODEL_FILENAME = os.path.join(os.path.split(__file__)[0], 'uncertainty-svm.p')
+
+CLASSIFIER = _pickle.load(open(MODEL_FILENAME, 'rb'))
+
+def score(document):
+    """
+    Given a document, calculate it's uncertainty against the pre-trained model.
+
+    Input: [{'text': "I am the walrus."}]
+    Output: {'uncertain': confidence, 'not uncertain': confidence}
+    """
+    global CLASSIFIER
+    feature_set = get_features(document)
+    feature_vector = [feature_set[f] for f in sorted(feature_set.keys())]
+    x = csr_matrix(np.asarray([feature_vector]))
+    probs = CLASSIFIER.predict_proba(x)
+
+    probs = {'uncertain': probs[0][1], 'not_uncertain': probs[0][0]}
+
+    return probs
+
 if __name__ == "__main__":
     args = sys.argv[1:]
-    if len(args) != 1:
+    if len(args) < 1:
         sys.exit(1)
     else:
         if args[0] == "wiki":
@@ -24,4 +51,43 @@ if __name__ == "__main__":
         elif args[0] == "features":
             with open(WIKI_NEW, 'r') as f:
                 sents = json.loads(f.read())
-                print(features(sents))
+                features(sents)
+        elif args[0] == "train":
+            print("Gathering all available training data....")
+            all_docs = []
+            all_docs += json.loads(open(WIKI_NEW, 'r').read())
+            all_docs += json.loads(open(FACTBANK_NEW, 'r').read())
+            all_docs += json.loads(open(BIO_BMC_NEW, 'r').read())
+            all_docs += json.loads(open(BIO_FLY_NEW, 'r').read())
+            all_docs += json.loads(open(BIO_HBC_NEW, 'r').read())
+            print(len(all_docs))
+
+            print("Starting to train model...")
+            FITTED_SVC = train_svm(all_docs, ntesting=int(args[1]))
+
+            print("Dumping model to disk...")
+            _pickle.dump(FITTED_SVC, open("uncertainty-svm.p", "wb"))
+
+            print("Tidying up...")
+        elif args[0] == "classify":
+            documents = []
+            with open(args[1], 'r') as doc:
+                for line in doc.readlines():
+                    documents.append({'text': line, 'ccue': {}})
+
+            uncertain = []
+            not_uncertain = []
+            for i, document in enumerate(documents):
+                probs = score(document)
+                uncertain.append(probs['uncertain'])
+                not_uncertain.append(probs['not_uncertain'])
+
+                print("\n==== Sentence " + str(i) + ":")
+                print("==== " + str(document['text']))
+                print("\tP(uncertain) = %.3f" % probs['uncertain'])
+                print("\tP(not_uncertain) = %.3f" % probs['not_uncertain'])
+
+            print("\n==== Document:")
+            print("\tP(uncertain) = %.3f" % np.mean(uncertain))
+            print("\tP(not_uncertain) = %.3f" % np.mean(not_uncertain))
+

scripts/train.py
@@ -5,49 +5,62 @@ import os
 import random
 import sys
 
-import features as f
+from sklearn import svm
+from scipy.sparse import csr_matrix
+from sklearn.metrics import classification_report, confusion_matrix
 
-def is_hedge(ngram, size):
-    if size == 3:
-        if " ".join(ngram) in TRI_HEDGES:
-            return True
-    elif size == 2:
-        if " ".join(ngram) in BI_HEDGES:
-            return True
-    elif size == 1:
-        if ngram in UNI_HEDGES:
-            return True
-    else:
-        raise ValueError("Invalid value for 'size': " + str(size))
+from corpora import WIKI_NEW, FACTBANK_NEW, BIO_BMC_NEW, \
+                    BIO_FLY_NEW, BIO_HBC_NEW
+from features import *
+from features.features import *
 
-    return False
+def train_svm(documents, ntesting=500):
 
-def is_weasel(ngram, size):
-    if size == 3:
-        if " ".join(ngram) in TRI_WEASELS:
-            return True
-    elif size == 2:
-        if " ".join(ngram) in BI_WEASELS:
-            return True
-    elif size == 1:
-        if ngram in UNI_WEASELS:
-            return True
-    else:
-        raise ValueError("Invalid value for 'size': " + str(size))
+    random.shuffle(documents)
+    testing = documents[-ntesting:]
+    documents = documents[:-ntesting]
 
-    return False
+    x, y = docs_to_feature_vectors(documents)
+    xtest, ytest = docs_to_feature_vectors(testing)
 
-def is_peacock(ngram, size):
-    if size == 3:
-        if " ".join(ngram) in TRI_PEACOCKS:
-            return True
-    elif size == 2:
-        if " ".join(ngram) in BI_PEACOCKS:
-            return True
-    elif size == 1:
-        if ngram in UNI_PEACOCKS:
-            return True
-    else:
-        raise ValueError("Invalid value for 'size': " + str(size))
+    classifier = svm.SVC(C=0.02, kernel='linear', probability=True)
+    classifier.fit(x, y)
 
-    return False
+    y_prediction = classifier.predict(xtest)
+#    print(xtest)
+#    print(ytest)
+    print(classification_report(ytest, y_prediction))
+    print("\n")
+    print(confusion_matrix(ytest, y_prediction))
+
+    return classifier
+
+
+def docs_to_feature_vectors(documents):
+    feature_keys = False
+    x, y = [], []
+    cnt = 0
+
+    for doc in documents:
+        feature_set = get_features(doc)
+        if not feature_keys:
+            feature_keys = sorted(feature_set.keys())
+
+        feature_vector = [feature_set[feature] for feature in feature_keys]
+
+        doc_class = 0
+        if len(doc['ccue']) > 0:
+            doc_class = 1
+
+#        print(doc_class)
+        x.append(feature_vector)
+        y.append(doc_class)
+        print(cnt)
+        cnt+=1
+
+    x = csr_matrix(np.asarray(x))
+    y = np.asarray(y)
+
+#    print(x)
+#    print(y)
+    return x, y