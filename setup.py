6fe21f44dc3cee4d0878b19201859a9861be5732
Benjamin S. Meyers <ben@lionlogic.org>
2017-05-18 09:03:11 -0400

    Updating setup.py to download the package from GitHub: - PyPi has an undisclosed limit on the size of a project they are willing to   host, and our data files are too big. Telling PyPi to download the package   from GitHub is a common workaround.

--- setup.py
+++ setup.py
@@ -13,12 +13,29 @@ setup(
             'scikit-learn==0.18.1',
             'nltk==3.2.2'
         ],
-        license='The MIT License (MIT) Copyright (c) 2017 Benjamin Meyers',
+        license='The MIT License (MIT) Copyright (c) 2017 Benjamin S. Meyers',
         description='Python implementation of a classifier for linguistic '
                     'uncertainty, based on the work by Vincze et al.',
-        author='Benjamin Meyers',
+        author='Benjamin S. Meyers',
         author_email='bsm9339@rit.edu',
         url='https://github.com/meyersbs/uncertainty',
+        download_url='https://github.com/meyersbs/uncertainty/uncertainty/dist/uncertainty-0.1.0.tar.gz',
         test_suite='uncertainty.tests',
-        classifiers=[]
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering :: Information Analysis',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Text Processing :: Linguistic'
+        ],
+        keywords=[
+            'nlp', 'natural language', 'natural language processing',
+            'uncertainty', 'linguistic uncertainty', 'classification'
+        ]
     )
