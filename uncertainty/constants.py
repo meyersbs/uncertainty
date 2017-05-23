bb5a0583926f177c3a64b400d3f4a040686fa194
Benjamin S. Meyers <ben@lionlogic.org>
2017-05-23 12:09:43 -0400

    Removed legacy models/vectorizers.

--- uncertainty/constants.py
+++ uncertainty/constants.py
@@ -1,33 +1,5 @@
-import pkg_resources
-
 from pkg_resources import resource_filename
 
 BCLASS_CLASSIFIER_PATH = resource_filename('uncertainty', 'models/bclass.p')
 MCLASS_CLASSIFIER_PATH = resource_filename('uncertainty', 'models/mclass.p')
 VECTORIZER_PATH = resource_filename('uncertainty', 'vectorizers/vectorizer.p')
-
-DATA_FILE_PATH = 'uncertainty/data/merged_data'
-BIN_CUE_MODEL_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'models/bclass.p'
-    )
-BIN_CUE_VECTORIZER_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'vectorizers/vectorizer.p'
-    )
-BIN_SENT_MODEL_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'models/binary-sent-model.p'
-    )
-BIN_SENT_VECTORIZER_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'vectorizers/binary-sent-vectorizer.p'
-    )
-MULTI_CUE_MODEL_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'models/mclass.p'
-    )
-MULTI_CUE_VECTORIZER_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'vectorizers/multiclass-cue-vectorizer.p'
-    )
-MULTI_SENT_MODEL_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'models/multiclass-sent-model.p'
-    )
-MULTI_SENT_VECTORIZER_PATH = pkg_resources.resource_filename(
-        'uncertainty', 'vectorizers/multiclass-sent-vectorizer.p'
-    )
